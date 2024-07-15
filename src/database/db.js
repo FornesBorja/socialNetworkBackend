5726805784b19e31fb2363f079a99ec5ca5e6f15
“FornesBorja” <fornes.borja@gmail.com>
2024-07-15 18:12:00 +0200

    feat: basic structure needed to make the connection to the DB

---
+++ src/database/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+    console.log('Start connection');
+      return mongoose.connect(
+      process.env.MONGO_URI,
+      {}
+    );
+  };
---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import "dotenv/config";
+import { dbConnection } from "./database/db.js";
+import { router } from "./router.js"
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json())
+
+app.get("/healthy", (req, res) => {
+	res.json({
+		success: true,
+		message: "Server is healthy",
+	});
+});
+
+
+app.use('/api/v1', router)
+
+dbConnection()
+.then(() => {
+  console.log('Database Connected');
+  app.listen(PORT, () => {
+    console.log(`Server running ${PORT}`);
+  });
+})
+.catch(error => {
+  console.log('Error connection database: ' + error);
+})
